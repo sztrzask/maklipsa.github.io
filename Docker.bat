c08e54c2f2447cc3a065c78b214d2f489978578c
maklipsa <maklipsa@gmail.com>
2018-06-27 09:32:11 +0200

    Docker file change

--- Docker.bat
+++ Docker.bat
@@ -1,2 +1,2 @@
 docker rm -f indexOutOfRange
-docker run -it -p 4000:4000 -v //d/src/IndexOutOfRange/:/site --name indexOutOfRange andredumas/github-pages serve --watch --force_polling
+docker run -it -p 4000:4000 -v //c/src/IndexOutOfRange/:/site --name indexOutOfRange andredumas/github-pages serve --watch --force_polling
